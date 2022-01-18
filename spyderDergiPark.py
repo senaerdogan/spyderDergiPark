a407d5cfdc7008f94f297ea86160567a99f9a082
senaerdogan <saadetsenaerdogan@gmail.com>
2022-01-18 19:53:26 +0300

    -fix file name problem -save with doi number (if exists) -fix url bug -add search with topic and search phrase -customize downloading path

--- spyderDergiPark.py
+++ spyderDergiPark.py
@@ -2,51 +2,159 @@ import pickle
 import time
 import requests
 from selenium import webdriver
+from selenium.webdriver import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
-
-with open('topicLinkDict.pickle', 'rb') as handle:
-    topicLinkDict = pickle.load(handle)
+from pathlib import Path
 
 
-options = Options()
-options.headless = True
-s = Service("chromedriver.exe")
-#for headless start: webdriver.Chrome( options= options, service=s)
-driver = webdriver.Chrome(  service=s)
+def loadTopicPickle():
+    with open('topicLinkDict.pickle', 'rb') as handle:
+        topicLinkDict = pickle.load(handle)
+    return topicLinkDict
 
-driver.get(topicLinkDict['hukuk'])
-time.sleep(5)
 
-numberOfPages = int(driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div/div/div/div[2]/div[2]/div[2]/div[3]/ul/li[8]/a').get_attribute("textContent"))
-currentPage = 1
-numberOfArticlesPerPage = len(driver.find_elements(By.XPATH, '//*[@id="kt_content"]/div[2]/div[2]/div[2]/div[2]/div'))
+def loadDriver(headless):
+    options = Options()
+    options.headless = True
+    s = Service("chromedriver.exe")
 
-while (currentPage <= numberOfPages):
-    article = 1
-    while (article <= numberOfArticlesPerPage):
-        driver.find_element(By.XPATH,'//*[@id="kt_content"]/div[2]/div[2]/div[2]/div[2]/div[{article:d}]/div/h5/a'.format(article=article)).click()
-        time.sleep(2)
-        language = "".join(driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div[2]/div[1]/div[2]/div/table/tbody/tr[1]/td").get_attribute("textContent").lower().split())
-        if (language == "türkçe"):
-            pdfLink = driver.find_element(By.XPATH, '//*[@id="article-toolbar"]/a[1]').get_attribute("href")
-            print(pdfLink)
-            response = requests.get(pdfLink)
-            #save with doi number
-            pdf = open("./articles/{article:d}.pdf".format(article= currentPage*article), 'wb')
-            pdf.write(response.content)
-            pdf.close()
-            print("File downloaded")
-            time.sleep(5)
+    if headless:
+        driver = webdriver.Chrome(options=options, service=s)
+    else:
+        driver = webdriver.Chrome(service=s)
+    return driver
+
+
+def getNumberOfArticlesToDownload(driver, maxArticle):
+    if maxArticle is None:
+        # total articles
+        return int("".join(
+            driver.find_element(By.XPATH, '//*[@id="search-sections"]/div[1]/a/span[2]/span[2]').get_attribute(
+                "textContent").split(".")))
+    return maxArticle
+
+
+def definePath(path):
+    if path is None:
+        return "articles"
+    return path
+
+
+def download(driver, maxArticle, language, current_url, path):
+    path = definePath(path)
+    Path(path).mkdir(parents=True, exist_ok=True)
+    numberOfPages = int(driver.find_element(By.XPATH,
+                                            '/html/body/div[2]/div/div/div/div/div/div/div[2]/div[2]/div[2]/div[3]/ul/li[8]/a').get_attribute(
+        "textContent"))
+    currentPage = 1
+    numberOfArticlesPerPage = len(
+        driver.find_elements(By.XPATH, '//*[@id="kt_content"]/div[2]/div[2]/div[2]/div[2]/div'))
+
+    numberOfArticlesToDownload = getNumberOfArticlesToDownload(driver, maxArticle)
+    numberOfDownloaded = 0
+
+    while currentPage <= numberOfPages:
+        article = 1
+        while article <= numberOfArticlesPerPage:
+
+            driver.find_element(By.XPATH,
+                                '//*[@id="kt_content"]/div[2]/div[2]/div[2]/div[2]/div[{article:d}]/div/h5/a'.format(
+                                    article=article)).click()
+            time.sleep(2)
+            languageArticle = "".join(driver.find_element(By.XPATH,
+                                                          '//*[@id="kt_content"]/div/div[2]/div[1]/div[2]/div/table/tbody/tr[1]/td').get_attribute(
+                "textContent").lower().split())
+            if languageArticle == language:
+                pdfLink = driver.find_element(By.XPATH, '//*[@id="article-toolbar"]/a[1]').get_attribute("href")
+                print(pdfLink)
+                response = requests.get(pdfLink)
+                try:
+                    # doi number
+                    doiLink = driver.find_element(By.XPATH, '//*[@id="article_en"]/div[1]/a').get_attribute(
+                        "textContent").split("/")
+                    articleFileName = "-".join(doiLink[-2:])
+                except NoSuchElementException:
+                    articleFileName = str(currentPage) + "-" + str(article)
+                pdf = open("./{path:s}/{article:s}.pdf".format(path=path, article=articleFileName), 'wb')
+                pdf.write(response.content)
+                pdf.close()
+                print("File downloaded")
+                time.sleep(3)
+                numberOfDownloaded += 1
+
             driver.execute_script("window.history.go(-1)")
             time.sleep(1)
-        article += 1
-    currentPage += 1
-    driver.get("https://dergipark.org.tr/tr/search/{PAGE:d}?section=articles&aggs%5Bsubjects.id%5D%5B0%5D=321".format(PAGE= currentPage))
+            article += 1
+            if numberOfDownloaded == numberOfArticlesToDownload:
+                break
+        if numberOfDownloaded == numberOfArticlesToDownload:
+            break
+        currentPage += 1
+        new_page = current_url.replace("search", "search/{PAGE:d}".format(PAGE=currentPage))
 
-    if currentPage == 4:
-        break
+        driver.get(new_page)
 
-driver.quit()
+
+def searchByTopic(topic, maxArticle=None, headless=False, language="türkçe"):
+    language = "".join(language.lower().split())
+    topic = "".join(topic.lower().split())
+    driver = loadDriver(headless)
+    topicLinkDict = loadTopicPickle()
+    driver.get(topicLinkDict[topic])
+    time.sleep(5)
+
+    current_url = driver.current_url
+    return driver, current_url
+
+
+def searchByPhrase(searchPhrase, maxArticle=None, headless=False, language="türkçe"):
+    language = "".join(language.lower().split())
+    driver = loadDriver(headless)
+    driver.get('https://dergipark.org.tr/tr/search?q=&section=articles')
+    time.sleep(5)
+
+    searchBar = driver.find_element(By.XPATH, '//*[@id="search-form"]/div/input')
+    searchBar.send_keys(searchPhrase)
+    time.sleep(2)
+
+    searchBar.send_keys(Keys.ENTER)
+    time.sleep(1)
+    current_url = driver.current_url
+    return driver, current_url
+
+
+def searchByTopicAndPhrase(topic, searchPhrase, maxArticle=None, headless=False, language="türkçe"):
+    language = "".join(language.lower().split())
+    topic = "".join(topic.lower().split())
+    driver = loadDriver(headless)
+    topicLinkDict = loadTopicPickle()
+    driver.get(topicLinkDict[topic])
+    time.sleep(5)
+
+    searchBar = driver.find_element(By.XPATH, '//*[@id="search-form"]/div/input')
+    searchBar.send_keys(searchPhrase)
+    time.sleep(2)
+
+    searchBar.send_keys(Keys.ENTER)
+    time.sleep(1)
+    current_url = driver.current_url
+    driver.get(current_url)
+    return driver, current_url
+
+
+def searchAndDownload(topic=None, searchPhrase=None, maxArticle=None, headless=False, language="türkçe", path=None):
+    if (topic is None) and (searchPhrase is None):
+        raise Exception("At least one of the topic or search phrase must be specified!")
+    elif (topic is not None) and (searchPhrase is not None):
+        driver, current_url = searchByTopicAndPhrase(topic, searchPhrase, maxArticle, headless, language)
+        download(driver, maxArticle, language, current_url, path)
+    elif topic is not None:
+        driver, current_url = searchByTopic(topic, maxArticle, headless, language)
+        download(driver, maxArticle, language, current_url, path)
+    elif searchPhrase is not None:
+        driver, current_url = searchByPhrase(searchPhrase, maxArticle, headless, language)
+        download(driver, maxArticle, language, current_url, path)
+
