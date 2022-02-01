8a7fecfae161cdd98bad0434255d34928c5ff050
senaerdogan <saadetsenaerdogan@gmail.com>
2022-02-01 22:38:42 +0300

    add progress bar and optional console logging

--- src/spyderDergiPark/spyderDergiPark.py
+++ src/spyderDergiPark/spyderDergiPark.py
@@ -8,7 +8,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from pathlib import Path
-from .createTopicLinkDict import createTopicLinkDict
+from createTopicLinkDict import createTopicLinkDict
+from tqdm import tqdm
 
 
 def defineDriverPath(path):
@@ -62,7 +63,7 @@ def getNumberOfArticlesToDownload(driver, maxArticle):
     return maxArticle
 
 
-def download(driver, maxArticle, language, current_url, path):
+def download(driver, maxArticle, language, current_url, path, progress, console_logging):
     path = defineOutputPath(path)
     Path(path).mkdir(parents=True, exist_ok=True)
     numberOfPages = int(driver.find_element(By.XPATH,
@@ -73,48 +74,62 @@ def download(driver, maxArticle, language, current_url, path):
         driver.find_elements(By.XPATH, '//*[@id="kt_content"]/div[2]/div[2]/div[2]/div[2]/div'))
 
     numberOfArticlesToDownload = getNumberOfArticlesToDownload(driver, maxArticle)
+    if progress:
+        pbar = tqdm(total=numberOfArticlesToDownload)
     numberOfDownloaded = 0
 
     while currentPage <= numberOfPages:
         article = 1
         while article <= numberOfArticlesPerPage:
+            number_of_try = 0
+            try:
+                driver.find_element(By.XPATH,
+                                    '//*[@id="kt_content"]/div[2]/div[2]/div[2]/div[2]/div[{article:d}]/div/h5/a'.format(
+                                        article=article)).click()
+                time.sleep(2)
+                languageArticle = "".join(driver.find_element(By.XPATH,
+                                                              '//*[@id="kt_content"]/div/div[2]/div[1]/div[2]/div/table/tbody/tr[1]/td').get_attribute(
+                    "textContent").lower().split())
+                if languageArticle == language:
+                    pdfLink = driver.find_element(By.XPATH, '//*[@id="article-toolbar"]/a[1]').get_attribute("href")
+                    if console_logging:
+                        print(pdfLink)
+                    response = requests.get(pdfLink)
+                    try:
+                        # doi number
+                        doiLink = driver.find_element(By.XPATH, '//*[@id="article_en"]/div[1]/a').get_attribute(
+                            "textContent").split("/")
+                        articleFileName = "-".join(doiLink[-2:])
+                    except NoSuchElementException:
+                        articleFileName = str(currentPage) + "-" + str(article)
+                    pdf = open("./{path:s}/{article:s}.pdf".format(path=path, article=articleFileName), 'wb')
+                    pdf.write(response.content)
+                    pdf.close()
+                    if console_logging:
+                        print("File downloaded")
+                    time.sleep(3)
+                    numberOfDownloaded += 1
+                    if progress:
+                        pbar.update(1)
 
-            driver.find_element(By.XPATH,
-                                '//*[@id="kt_content"]/div[2]/div[2]/div[2]/div[2]/div[{article:d}]/div/h5/a'.format(
-                                    article=article)).click()
-            time.sleep(2)
-            languageArticle = "".join(driver.find_element(By.XPATH,
-                                                          '//*[@id="kt_content"]/div/div[2]/div[1]/div[2]/div/table/tbody/tr[1]/td').get_attribute(
-                "textContent").lower().split())
-            if languageArticle == language:
-                pdfLink = driver.find_element(By.XPATH, '//*[@id="article-toolbar"]/a[1]').get_attribute("href")
-                print(pdfLink)
-                response = requests.get(pdfLink)
-                try:
-                    # doi number
-                    doiLink = driver.find_element(By.XPATH, '//*[@id="article_en"]/div[1]/a').get_attribute(
-                        "textContent").split("/")
-                    articleFileName = "-".join(doiLink[-2:])
-                except NoSuchElementException:
-                    articleFileName = str(currentPage) + "-" + str(article)
-                pdf = open("./{path:s}/{article:s}.pdf".format(path=path, article=articleFileName), 'wb')
-                pdf.write(response.content)
-                pdf.close()
-                print("File downloaded")
-                time.sleep(3)
-                numberOfDownloaded += 1
+                driver.execute_script("window.history.go(-1)")
+                time.sleep(1)
+                article += 1
+                if numberOfDownloaded == numberOfArticlesToDownload:
+                    break
+            except Exception:
+                if number_of_try == 3:
+                    continue
+                number_of_try += 1
+                time.sleep(1)
 
-            driver.execute_script("window.history.go(-1)")
-            time.sleep(1)
-            article += 1
-            if numberOfDownloaded == numberOfArticlesToDownload:
-                break
         if numberOfDownloaded == numberOfArticlesToDownload:
             break
         currentPage += 1
         new_page = current_url.replace("search", "search/{PAGE:d}".format(PAGE=currentPage))
 
         driver.get(new_page)
+    pbar.close()
 
 
 def searchByTopic(topic, maxArticle=None, headless=False, language="türkçe", driverPath=None):
@@ -166,16 +181,17 @@ def searchByTopicAndPhrase(topic, searchPhrase, maxArticle=None, headless=False,
     return driver, current_url
 
 
-def searchAndDownload(topic=None, searchPhrase=None, maxArticle=None, headless=False, language="türkçe", path=None):
+def searchAndDownload(topic=None, searchPhrase=None, maxArticle=None, headless=True, language="türkçe", path=None,
+                      progress=True, console_logging=False, driver_path=None):
     if (topic is None) and (searchPhrase is None):
         raise Exception("At least one of the topic or search phrase must be specified!")
     elif (topic is not None) and (searchPhrase is not None):
-        driver, current_url = searchByTopicAndPhrase(topic, searchPhrase, maxArticle, headless, language)
-        download(driver, maxArticle, language, current_url, path)
+        driver, current_url = searchByTopicAndPhrase(topic, searchPhrase, maxArticle, headless, language, driver_path)
+        download(driver, maxArticle, language, current_url, path, progress, console_logging)
     elif topic is not None:
-        driver, current_url = searchByTopic(topic, maxArticle, headless, language)
-        download(driver, maxArticle, language, current_url, path)
+        driver, current_url = searchByTopic(topic, maxArticle, headless, language, driver_path)
+        download(driver, maxArticle, language, current_url, path, progress, console_logging)
     elif searchPhrase is not None:
-        driver, current_url = searchByPhrase(searchPhrase, maxArticle, headless, language)
-        download(driver, maxArticle, language, current_url, path)
+        driver, current_url = searchByPhrase(searchPhrase, maxArticle, headless, language, driver_path)
+        download(driver, maxArticle, language, current_url, path, progress, console_logging)
 
